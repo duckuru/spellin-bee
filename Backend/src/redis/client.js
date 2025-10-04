f5bfedffcb15cdd2250ac1d096997a900bd3bd57
duckuru <sivlong42069@gmail.com>
2025-10-04 10:27:09 +0700

    redisServer

--- Backend/src/redis/client.js
+++ Backend/src/redis/client.js
@@ -1,8 +1,8 @@
 import Redis from "ioredis";
+import {ENV} from "./env.js";
 
-const redis = new Redis({
-  host: "127.0.0.1",
-  port: 6379,
+const redis = new Redis(  ENV.REDIS_URL,{
+  tls: {},
 });
 
 export default redis;
