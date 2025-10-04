314ee9c47bf4a49d429a0052c10db4afe162b764
duckuru <sivlong42069@gmail.com>
2025-10-04 12:09:11 +0700

    test2

--- Backend/src/redis/client.js
+++ Backend/src/redis/client.js
@@ -1,7 +1,12 @@
 import Redis from "ioredis";
 
-const redis = new Redis(process.env.REDIS_URL,{
-  tls: {},
-});
+const isProduction = process.env.NODE_ENV === "production";
+
+const redis = isProduction
+  ? new Redis(process.env.REDIS_URL) // Connects via TLS automatically
+  : new Redis("redis://127.0.0.1:6379"); // Local dev
+
+redis.on("connect", () => console.log("Redis connected!"));
+redis.on("error", (err) => console.error("Redis error:", err));
 
 export default redis;
