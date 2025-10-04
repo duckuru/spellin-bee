dd8a7307204be5947b19281dacde3ed7858ac557
duckuru <sivlong42069@gmail.com>
2025-10-04 14:04:11 +0700

    bruh

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -24,11 +24,12 @@ app.use("/api", resultRoutes);
 
 
 if (ENV.NODE_ENV == "production") {
-  app.use(express.static(path.join(__dirname, "frontend/dist")));
+  app.use(express.static(path.join(__dirname, "../../frontend/dist")));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "frontend/dist", "index.html"));
+    res.sendFile(path.join(__dirname, "../../frontend/dist/index.html"));
   });
+
 }
 
 
