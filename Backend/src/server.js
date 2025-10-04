47f049e3264432f68187011b93316b03a1d67a95
duckuru <sivlong42069@gmail.com>
2025-10-04 13:53:50 +0700

    brooooo

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -23,12 +23,11 @@ app.use("/api/auth", authRoutes);
 app.use("/api", resultRoutes);
 
 
-if (ENV.NODE_ENV === "production") {
-  const frontendDist = path.join(__dirname, "frontend/dist");
-  app.use(express.static(frontendDist));
+if (ENV.NODE_ENV == "production") {
+  app.use(express.static(path.join(__dirname, "frontend/dist")));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.join(frontendDist, "index.html"));
+    res.sendFile(path.join(__dirname, "frontend/dist", "index.html"));
   });
 }
 
