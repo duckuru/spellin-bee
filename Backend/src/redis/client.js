1c7227fd338caaa9babc08ec2b88f0b7c0ee78e7
duckuru <sivlong42069@gmail.com>
2025-10-04 11:54:01 +0700

    buh

--- Backend/src/redis/client.js
+++ Backend/src/redis/client.js
@@ -1,16 +1,7 @@
 import Redis from "ioredis";
 
-const isProduction = process.env.NODE_ENV === "production";
-
-const redis = new Redis({
-  host: isProduction ? process.env.REDIS_HOST : "127.0.0.1",
-  port: isProduction ? Number(process.env.REDIS_PORT) : 6379,
-  username: isProduction ? process.env.REDIS_USER : undefined,
-  password: isProduction ? process.env.REDIS_PASS : undefined,
-  tls: isProduction ? {} : undefined
+const redis = new Redis(process.env.REDIS_URL,{
+  tls: {},
 });
-
-redis.on("connect", () => console.log("Redis connected!"));
-redis.on("error", (err) => console.error("Redis error:", err));
 
 export default redis;
