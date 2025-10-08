c0e774c4bc5eec6ebea0c8258cfb6ae45242efb7
duckuru <sivlong42069@gmail.com>
2025-10-08 17:15:51 +0700

    fix safari cookie error

--- Backend/src/lib/utils.js
+++ Backend/src/lib/utils.js
@@ -15,7 +15,7 @@ export const generateToken = (userId, res) => {
   res.cookie("jwt", token, {
     maxAge: 7 * 24 * 60 * 60 * 1000, //MS
     httpOnly: true, //prevent XSS attack || cross site scripting
-    sameSite: "strict", // prevent CSRF attacks
+    sameSite: NODE_ENV === "development" ? "lax" : "strict", // allow local dev
     secure: NODE_ENV === "development" ? false : true,
   });
 
