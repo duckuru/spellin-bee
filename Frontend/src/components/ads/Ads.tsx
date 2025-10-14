52a12ff18ef7cd72ce16268d3ffaec72f04234e2
duckuru <sivlong42069@gmail.com>
2025-10-14 16:59:00 +0700

    fix unused

--- Frontend/src/components/ads/Ads.tsx
+++ Frontend/src/components/ads/Ads.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useRef } from 'react'
+import { useEffect, useState, useRef } from 'react'
 
 export default function Ads() {
   const [isOpen, setIsOpen] = useState(true);
